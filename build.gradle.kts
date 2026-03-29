c874fe400b2518f790b100648c55cdba38f2cc98
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-03-29 14:13:39 -0300

    java spec for gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,3 +14,12 @@ dependencies {
     compileOnly("org.spigotmc:spigot-api:1.21.9-R0.1-SNAPSHOT")
     implementation(files("lib/Barrel-1.0-SNAPSHOT.jar"))
 }
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+
+    withSourcesJar()
+}
